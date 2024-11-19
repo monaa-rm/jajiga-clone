81515400bb0a98bb0d61633ed3db401434bbdc71
monaa-rm <83189980+monaa-rm@users.noreply.github.com>
2024-11-19 18:51:36 +0330

    Update page.jsx

--- src/app/s/page.jsx
+++ src/app/s/page.jsx
@@ -2,17 +2,21 @@ import SearchPage from "@/components/templates/search-page";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import User from "../../../models/User";
+import connectDB from "../../../utils/connectDb";
 
 const SearchSection = async () => {
   const session = await getServerSession(authOptions);
   let liked_items = [];
-  if (session) {
-   const user = await User.findOne({ phone: session?.user?.phone }).select({
-      liked_residances: 1,
-    });
-    liked_items = user?.liked_residances ?? []
-  }else {
-     liked_items = JSON.parse(localStorage.getItem("likedResidances")) || [];
+  try {
+    await connectDB();
+    if (session) {
+      const user = await User.findOne({ phone: session?.user?.phone }).select({
+        liked_residances: 1,
+      });
+      liked_items = user?.liked_residances ?? [];
+    }
+  } catch (error) {
+    liked_items = [];
   }
 
   return (
