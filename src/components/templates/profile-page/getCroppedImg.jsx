ee2e32ac9b171d9eac7785b7e122e9225abd2a2e
monaa-rm <83189980+monaa-rm@users.noreply.github.com>
2024-12-24 04:24:17 +0330

    Update getCroppedImg.jsx

--- src/components/templates/profile-page/getCroppedImg.jsx
+++ src/components/templates/profile-page/getCroppedImg.jsx
@@ -13,55 +13,54 @@ const createImage = (url) =>
 const getRadianAngle = (degreeValue) => (degreeValue * Math.PI) / 180;
 
 export const getCroppedImg = async (imageSrc, pixelCrop, rotation = 0) => {
-  const image = await createImage(imageSrc); // استفاده از await برای بارگذاری تصویر
+    const image = await createImage(imageSrc); // استفاده از await برای بارگذاری تصویر
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
-    
-    // مقیاس دادن به ابعاد تصویر
+
+  // مقیاس دادن به ابعاد تصویر
     const scaledWidth = image.width / 2;
     const scaledHeight = image.height / 2;
+    
+  const safeArea = Math.max(image.width, image.height) * 2;
 
-    const safeArea = Math.max(scaledWidth, scaledHeight) * 2;
-  
+
     canvas.width = safeArea;
     canvas.height = safeArea;
   
     ctx.translate(safeArea / 2, safeArea / 2);
     ctx.rotate(getRadianAngle(rotation));
     ctx.translate(-safeArea / 2, -safeArea / 2);
-
+    
     ctx.drawImage(
       image,
-      safeArea / 2 - scaledWidth, // استفاده از ابعاد مقیاس شده
-      safeArea / 2 - scaledHeight, // استفاده از ابعاد مقیاس شده
-      scaledWidth,  // ابعاد مقیاس شده
-      scaledHeight, // ابعاد مقیاس شده
+      safeArea / 2 - image.width / 2,
+      safeArea / 2 - image.height / 2
     );
 
-    // دریافت دیتاهای پیکسل با استفاده از مقادیر تصحیح شده
-    const data = ctx.getImageData(
-        safeArea / 2 - scaledWidth + pixelCrop.x, // استفاده از ابعاد مقیاس شده
-        safeArea / 2 - scaledHeight + pixelCrop.y, // استفاده از ابعاد مقیاس شده
-        pixelCrop.width,
-        pixelCrop.height
-      );
-  
-    canvas.width = pixelCrop.width;
-    canvas.height = pixelCrop.height;
-  
+
+  const data = ctx.getImageData(
+    safeArea / 2 - image.width / 2  + pixelCrop.x,
+    safeArea / 2 - image.height / 2 + pixelCrop.y,
+    pixelCrop.width,
+    pixelCrop.height
+  );
+
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
     ctx.putImageData(data, 0, 0);
-  
-  return new Promise((resolve, reject) => {
-    canvas.toBlob((blob) => {
-        if (blob) {
-          const fileName = imageSrc.name;
-          const fileType = imageSrc.type;
-          const lastModified = imageSrc.lastModified;
-          const croppedFile = new File([blob], fileName, { type: fileType, lastModified });
-          resolve(croppedFile);
-        } else {
-          reject(new Error('Canvas is empty'));
-        }
-      }, 'image/jpeg');
-    });
-};
+
+    return new Promise((resolve, reject) => {
+      canvas.toBlob((blob) => {
+          if (blob) {
+            const fileName = imageSrc.name;
+            const fileType = imageSrc.type;
+            const lastModified = imageSrc.lastModified;
+            const croppedFile = new File([blob], fileName, { type: fileType, lastModified });
+            resolve(croppedFile);
+          } else {
+            reject(new Error('Canvas is empty'));
+          }
+        }, 'image/jpeg');
+      });
+  };
